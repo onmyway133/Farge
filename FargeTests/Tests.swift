1e772f351dfcee301927450f0a6f5be97a631d07
Khoa Pham <onmyway133@gmail.com>
2017-04-26 23:49:33 +0200

    Add Converter

--- FargeTests/Tests.swift
+++ FargeTests/Tests.swift
@@ -4,7 +4,7 @@ import XCTest
 class Tests: XCTestCase {
 
   func testRGB() {
-    let rgb = Parser().rgb(hex: "FF00FF")
+    let rgb = Converter().rgb(hex: "FF00FF")
     XCTAssertEqual(rgb.r, 1)
     XCTAssertEqual(rgb.g, 0)
     XCTAssertEqual(rgb.b, 1)
---
+++ Sources/Converter.swift
@@ -0,0 +1,39 @@
+import Foundation
+
+struct RGB {
+  let r: Float
+  let g: Float
+  let b: Float
+}
+
+struct HSL {
+  let h: Float
+  let s: Float
+  let l: Float
+}
+
+struct Converter {
+
+  func rgb(hex: String) -> RGB {
+    let r = decimal(string: hex, from: 0, length: 2)
+    let g = decimal(string: hex, from: 2, length: 2)
+    let b = decimal(string: hex, from: 4, length: 2)
+
+    return RGB(r: r/255, g: g/255, b: b/255)
+  }
+
+  func hsl(rgb: RGB) -> HSL {
+    return HSL(h: 0, s: 0, l: 0)
+  }
+
+  // MARK: - Helper
+
+  func decimal(string: String, from: Int, length: Int) -> Float {
+    let start = string.index(string.startIndex, offsetBy: from)
+    let end = string.index(start, offsetBy: length)
+    let range = Range(uncheckedBounds: (start, end))
+    let substring = string.substring(with: range)
+    let value = UInt8(substring, radix: 16) ?? 0
+    return Float(value)
+  }
+}
