5166f14611597c0116482def9ca0acb66b1340d1
Khoa Pham <onmyway133@gmail.com>
2017-04-26 22:57:42 +0200

    Add Parser

--- Sources/Parser.swift
+++ Sources/Parser.swift
@@ -1,9 +1,8 @@
-//
-//  Parser.swift
-//  Farge
-//
-//  Created by Khoa Pham on 26/04/2017.
-//  Copyright © 2017 Khoa Pham. All rights reserved.
-//
-
 import Foundation
+
+struct Parser {
+
+  func parse(hex: String) -> String {
+    return ""
+  }
+}
