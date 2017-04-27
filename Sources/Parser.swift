255664f50c6b983e912ff9d62360252dc2e7f161
Khoa Pham <onmyway133@gmail.com>
2017-04-27 13:42:22 +0200

    Update Parser.swift

--- Sources/Parser.swift
+++ Sources/Parser.swift
@@ -3,7 +3,7 @@ import Foundation
 struct Parser {
 
   func parse(hex: String) -> String? {
-    var chosenHex = ""
+    var name = ""
     var minDiff: Float = Float.greatestFiniteMagnitude
     let hsl1 = Converter().hsl(hex: hex)
 
@@ -12,10 +12,10 @@ struct Parser {
       let diff = pow(hsl1.h - hsl2.h, 2) + pow(hsl1.s - hsl2.s, 2) + pow(hsl1.l - hsl2.l, 2)
       if diff < minDiff {
         minDiff = diff
-        chosenHex = key
+        name = value
       }
     })
 
-    return mapping[chosenHex]
+    return name
   }
 }
